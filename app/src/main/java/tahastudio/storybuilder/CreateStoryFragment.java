5b56019d4d51a25c822f7ca95503a5fcbf81b39a
nuratun <nuratun@gmail.com>
2015-11-23 21:34:09 +0200

    Refactored

--- app/src/main/java/tahastudio/storybuilder/CreateStoryFragment.java
+++ app/src/main/java/tahastudio/storybuilder/CreateStoryFragment.java
@@ -1,8 +1,7 @@
 package tahastudio.storybuilder;
 
-import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,26 +18,6 @@ public class CreateStoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        getActivity().findViewById(R.id.add_characters).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(v.getContext(), AddCharacters.class));
-            }
-        });
-
-        getActivity().findViewById(R.id.add_plotline).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(v.getContext(), AddPlotline.class));
-            }
-        });
-
-        getActivity().findViewById(R.id.add_places).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(v.getContext(), AddPlaces.class));
-            }
-        });
 
         return inflater.inflate(R.layout.fragment_create_story, container, false);
     }
