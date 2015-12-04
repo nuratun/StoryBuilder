2438c1dba4c0494401603731e6c524f24170a6e2
nuratun <nuratun@gmail.com>
2015-12-04 21:04:58 +0200

    Replace fragment with second activity

--- app/src/main/java/tahastudio/storybuilder/CreateStory.java
+++ app/src/main/java/tahastudio/storybuilder/CreateStory.java
@@ -5,26 +5,32 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.FileOutputStream;
 
 public class CreateStory extends AppCompatActivity {
+    // Get the toolbar, second FAB button, and
+    // other elements
+    android.support.v7.widget.Toolbar toolbar;
     FloatingActionButton the_second_fab;
-    EditText story_title;
+    TextView story_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_story);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Find the toolbar and set the action bar
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Get the FAB for the story creation. Clicking on this FAB should bring a
         // small pop-up list with the ability to add a new character, plotline, or place
         the_second_fab = (FloatingActionButton) findViewById(R.id.second_fab);
 
         // Link to the editText id in fragment_create_story XML
-        story_title = (EditText) findViewById(R.id.edit_title);
+        story_title = (TextView) findViewById(R.id.story_title);
 
         // Filename to save to internal storage
         // TODO -> Implement the SQL database
--- app/src/main/java/tahastudio/storybuilder/StoryBuilderMain.java
+++ app/src/main/java/tahastudio/storybuilder/StoryBuilderMain.java
@@ -1,5 +1,6 @@
 package tahastudio.storybuilder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
@@ -112,7 +113,9 @@ public class StoryBuilderMain extends AppCompatActivity {
         add_the_story.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new CreateStory();
+                // Add an intent for CreateStory
+                Intent callCreateStory = new Intent(StoryBuilderMain.this, CreateStory.class);
+                startActivity(callCreateStory);
             }
         });
 
