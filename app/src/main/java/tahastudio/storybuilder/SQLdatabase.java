96929f1bd24c3f331890859afc788895f6f04125
nuratun <nuratun@gmail.com>
2015-12-11 19:11:17 +0200

    Start adding in the database

---
+++ app/src/main/java/tahastudio/storybuilder/SQLdatabase.java
@@ -0,0 +1,25 @@
+package tahastudio.storybuilder;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class SQLdatabase extends SQLiteDatabase {
+    // Version number must change if schema changes
+    public static final double DATABASE_VERSION = 1.0;
+    public static final String DATABASE_NAME = "sql.db";
+
+    // Set up database schema
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_TYPE = ",";
+
+    private static final String SQL_CREATE_ENTRIES = "(";
+
+    public SQLdatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+}
